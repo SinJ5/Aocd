f919bb3174742b479646db5236d5b9144ce40968
SinJ5 <svj@post.cz>
2019-12-09 20:12:58 +0100

    change 2019-Day2-A use int_code_computer.py

--- 2019/2019-Day2-A.py
+++ 2019/2019-Day2-A.py
@@ -1,35 +1,16 @@
+from aocd.models import Puzzle
+from int_code_computer import IntCodeComputer
 
-def readFile ( ):
-    ''' nacte vstupni soubor a prevede na list integeru'''
-    f = open("day2", "r")
-    Text = f.readlines()
-    f.close()
-    inputs = Text[0].split(",")
-    out=[]
-    for each in inputs:
-        out.append(int (each))
-    return out
+puzzle = Puzzle(year=2019, day=2)
 
-def doOpt(prog,op ,indexA,indexB,indexOut):
-    '''provede operaci  op nad daty z indexu A a B a vysledek ulozi do indexu Out '''
-    valA=prog[indexA]
-    valB=prog[indexB]
-    if(op==1):
-        prog[indexOut]=valA+valB
-    if(op==2):
-        prog[indexOut] = valA * valB
+prog = [int(x) for x in puzzle.input_data.split(",")]
 
+prog[1] = 12
+prog[2] = 2
+comp = IntCodeComputer(prog)
 
-def partA( val1 ,val2):
-    '''provede program se zadanym prametrm val1 a val2'''
-    prog = readFile()
-    actualIndex = 0
-    prog[1] = val1
-    prog[2] = val2
-    while prog[actualIndex] != 99:
-        doOpt(prog,prog[actualIndex], prog[actualIndex + 1], prog[actualIndex + 2], prog[actualIndex + 3])
-        actualIndex += 4
-    return prog[0]
+comp.parseCode([])
+result = comp.prog[0]
 
-print( "partA:",partA(12,2))
-
+print("2019-Day2-A result:", result)
+puzzle.answer_a = result
---
+++ 2019/int_code_computer.py
@@ -0,0 +1,112 @@
+class IntCodeComputer:
+
+    def __init__(self, code):
+        self.code = code
+        self.prog = code
+        self.step = 0
+        self.input_index = 0
+        self.output = 0
+        self.inputs = ()
+        self.stop = False
+        self.cmd = {1: self.sum,
+                    2: self.mult,
+                    3: self.input,
+                    4: self.out,
+                    5: self.if_true,
+                    6: self.if_false,
+                    7: self.if_less,
+                    8: self.if_equal,
+                    99: self.end
+                    }
+
+    def addstep(self):
+        self.step += 1
+
+    def reset(self):
+        self.prog = self.code
+
+    def next_input(self):
+        self.input_index += 1
+        if (self.input_index >= len(self.inputs)):
+            self.input_index = 0
+
+    def getValue(self, position_type):
+        if position_type == 0:
+            out = self.prog[self.prog[self.step]]
+        else:
+            out = self.prog[self.step]
+        self.addstep()
+        return out
+
+    def mult(self, op):
+        valA = self.getValue((op // 100) % 10)
+        valB = self.getValue((op // 1000) % 10)
+        self.prog[self.prog[self.step]] = valA * valB
+        self.addstep()
+
+    def sum(self, op):
+        valA = self.getValue((op // 100) % 10)
+        valB = self.getValue((op // 1000) % 10)
+        self.prog[self.prog[self.step]] = valA + valB
+        self.addstep()
+
+    def input(self, op):
+        self.prog[self.prog[self.step]] = self.inputs[self.input_index]
+        self.addstep()
+        self.next_input()
+
+    def out(self, op):
+        self.output = self.prog[self.prog[self.step]]
+        self.addstep()
+
+    def if_true(self, op):
+        valA = self.getValue((op // 100) % 10)
+        valB = self.getValue((op // 1000) % 10)
+        if valA == 0:
+            self.step = valB
+
+    def if_false(self, op):
+        valA = self.getValue((op // 100) % 10)
+        valB = self.getValue((op // 1000) % 10)
+        if valA != 0:
+            self.step = valB
+
+    def if_less(self, op):
+        valA = self.getValue((op // 100) % 10)
+        valB = self.getValue((op // 1000) % 10)
+        if (valA < valB):
+            self.prog[self.prog[self.step]] = 1
+        else:
+            self.prog[self.prog[self.step]] = 0
+        self.addstep()
+
+    def if_equal(self, op):
+        valA = self.getValue((op // 100) % 10)
+        valB = self.getValue((op // 1000) % 10)
+        if (valA < valB):
+            self.prog[self.prog[self.step]] = 1
+        else:
+            self.prog[self.prog[self.step]] = 0
+        self.addstep()
+
+    def end(self,op):
+        self.stop=True
+
+    def getNext(self):
+        out = self.prog[self.step]
+        self.addstep()
+        return out
+
+    def parseCode(self, inputs=[0]):
+        self.inputs = inputs
+        self.input_index = 0
+        self.step=0
+        self.stop=False
+
+        while not self.stop:
+            op =self.getNext()
+            self.cmd[op%100](op)
+
+        return self.output
+
+
