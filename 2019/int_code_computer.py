252bb398d6b1d94d97d0510a76efdf26841b3b10
SinJ5 <svj@post.cz>
2019-12-10 18:42:51 +0100

    change int_code_computer  add relative base, op 09, relative position of parametrers and all outputs list

--- 2019/int_code_computer.py
+++ 2019/int_code_computer.py
@@ -9,6 +9,7 @@ class IntCodeComputer:
         self.inputs = []
         self.stop = False
         self.reachEndofCode=False
+        self.relativeBase=0
         self.cmd = {1: self.sum,
                     2: self.mult,
                     3: self.input,
@@ -17,6 +18,7 @@ class IntCodeComputer:
                     6: self.if_false,
                     7: self.if_less,
                     8: self.if_equal,
+                    9: self.changeBase,
                     99: self.end
                     }
 
@@ -27,6 +29,7 @@ class IntCodeComputer:
         self.prog = self.code.copy()
         self.step=0
         self.inputs=[]
+        self.outputs=[]
         self.input_index=0
         self.reachEndofCode = False
 
@@ -36,35 +39,48 @@ class IntCodeComputer:
     def getValue(self, position_type):
         if position_type == 0:
             out = self.prog[self.prog[self.step]]
-        else:
+        elif(position_type==1):
             out = self.prog[self.step]
+        else :
+            out = self.prog[self.relativeBase+self.prog[self.step]]
         self.addstep()
         return out
+
+    def setValue(self, position_type, val):
+        if position_type == 0:
+            self.prog[self.prog[self.step]]=val
+        elif(position_type==1):
+            self.prog[self.step]=val
+        else :
+            self.prog[self.relativeBase+self.prog[self.step]]=val
+        self.addstep()
 
     def mult(self, op):
         valA = self.getValue((op // 100) % 10)
         valB = self.getValue((op // 1000) % 10)
-        self.prog[self.prog[self.step]] = valA * valB
-        self.addstep()
+        self.setValue((op // 10000) % 10, valA * valB)
+
 
     def sum(self, op):
         valA = self.getValue((op // 100) % 10)
         valB = self.getValue((op // 1000) % 10)
-        self.prog[self.prog[self.step]] = valA + valB
-        self.addstep()
+        self.setValue((op // 10000) % 10, valA + valB)
+
 
     def input(self, op):
         if(self.input_index>=len(self.inputs)):
             self.stop=True
             self.step-=1
             return
-        self.prog[self.prog[self.step]] = self.inputs[self.input_index]
-        self.addstep()
+        #self.prog[self.prog[self.step]] = self.inputs[self.input_index]
+        self.setValue((op // 100) % 10,self.inputs[self.input_index])
+
         self.input_index+=1
 
     def out(self, op):
-        self.output = self.prog[self.prog[self.step]]
-        self.addstep()
+        valA = self.getValue((op // 100) % 10)
+        self.output = valA
+        self.outputs+=[valA]
 
 
     def if_true(self, op):
@@ -83,19 +99,17 @@ class IntCodeComputer:
         valA = self.getValue((op // 100) % 10)
         valB = self.getValue((op // 1000) % 10)
         if (valA < valB):
-            self.prog[self.prog[self.step]] = 1
+            self.setValue((op // 10000) % 10,1)
         else:
-            self.prog[self.prog[self.step]] = 0
-        self.addstep()
+            self.setValue((op // 10000) % 10, 0)
 
     def if_equal(self, op):
         valA = self.getValue((op // 100) % 10)
         valB = self.getValue((op // 1000) % 10)
         if (valA == valB):
-            self.prog[self.prog[self.step]] = 1
+            self.setValue((op // 10000) % 10, 1)
         else:
-            self.prog[self.prog[self.step]] = 0
-        self.addstep()
+            self.setValue((op // 10000) % 10, 0)
 
     def end(self,op):
         self.stop=True
@@ -106,6 +120,10 @@ class IntCodeComputer:
         out = self.prog[self.step]
         self.addstep()
         return out
+
+    def changeBase(self,op):
+        valA = self.getValue((op // 100) % 10)
+        self.relativeBase+=valA
 
     def parseCode(self, inputs=[0],resetStep=True,appendInput=False):
         if(appendInput):
