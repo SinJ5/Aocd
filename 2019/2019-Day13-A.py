5692f76b4e422ca4562373925b1e795368830ba9
SinJ5 <svj@post.cz>
2019-12-15 09:14:29 +0100

    ADD: 2019-Day13-A

---
+++ 2019/2019-Day13-A.py
@@ -0,0 +1,29 @@
+from aocd.models import Puzzle
+from int_code_computer2 import IntCodeComputer2, STATUS_END_OF_PROGRAM, STATUS_STOP_ON_OUTPUT
+
+puzzle = Puzzle(year=2019, day=13)
+
+prog=[ int(x)  for x in puzzle.input_data.split(",") ]+[0 for i in range(0,6000)]
+comp = IntCodeComputer2(prog,True,False,False)
+
+comp.reset()
+
+output = []
+map ={}
+
+while comp.run() != STATUS_END_OF_PROGRAM:
+    if (comp.status == STATUS_STOP_ON_OUTPUT):
+        output.append(comp.output)
+        if (len(output) == 3):
+            map[(output[0],output[1])]=output[2]
+            output = []            
+            
+
+print(map)
+
+result=sum([1 for x in map.values() if x==2])
+
+
+print("2019-Day13-A result:", result)
+
+puzzle.answer_a = result
