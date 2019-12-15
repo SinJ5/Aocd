8b5b0d310347705ec427ea361929e881549167ae
SinJ5 <svj@post.cz>
2019-12-15 20:29:04 +0100

    change 2019-Day13-A use show map

--- 2019/2019-Day13-A.py
+++ 2019/2019-Day13-A.py
@@ -1,6 +1,6 @@
 from aocd.models import Puzzle
 from int_code_computer2 import IntCodeComputer2, STATUS_END_OF_PROGRAM, STATUS_STOP_ON_OUTPUT
-
+from ShowMap import ShowMap
 puzzle = Puzzle(year=2019, day=13)
 
 prog=[ int(x)  for x in puzzle.input_data.split(",") ]+[0 for i in range(0,6000)]
@@ -19,7 +19,10 @@ while comp.run() != STATUS_END_OF_PROGRAM:
             output = []            
             
 
-print(map)
+
+dmap=ShowMap({0:".",1:"8",2:"#",3:"=",4:"*"})
+dmap.makeFrame(map)
+dmap.draw()
 
 result=sum([1 for x in map.values() if x==2])
 
