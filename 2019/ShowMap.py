fcdb374c12eac392a8488139fb23859f3940af6c
SinJ5 <svj@post.cz>
2019-12-15 20:59:37 +0100

    change ShowMap add argument empty char

--- 2019/ShowMap.py
+++ 2019/ShowMap.py
@@ -4,8 +4,9 @@ Created on 15. 12. 2019
 @author: j
 '''
 class ShowMap:
-    def __init__(self,legend):
+    def __init__(self,legend,emptyChar=" "):
         self.legend=legend
+        self.emptyChr=emptyChar
     
     def makeFrame(self,data):
         tmpX=[pos[0] for pos in data.keys()]
@@ -14,7 +15,7 @@ class ShowMap:
         minY=min(tmpY)
         maxX=max(tmpX)
         maxY=max(tmpY)
-        self.frame =  [[" "for x in range(maxX-minX+2)] for y in range(maxY-minY+2)]
+        self.frame =  [[self.emptyChr for x in range(maxX-minX+2)] for y in range(maxY-minY+2)]
         for pos,val in data.items():
             self.frame[pos[1]-minY][pos[0]-minX]=self.legend[val]
     
