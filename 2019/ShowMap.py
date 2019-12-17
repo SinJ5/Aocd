a64f14f89c5132bd677d1dc23da73b045d6ea1c4
SinJ5 <svj@post.cz>
2019-12-17 09:20:04 +0100

    change showMap if legend None use chr

--- 2019/ShowMap.py
+++ 2019/ShowMap.py
@@ -7,6 +7,10 @@ class ShowMap:
     def __init__(self,legend,emptyChar=" "):
         self.legend=legend
         self.emptyChr=emptyChar
+        if legend is None:
+            self.useAscii=True
+        else:
+            self.useAscii=False
     
     def makeFrame(self,data):
         tmpX=[pos[0] for pos in data.keys()]
@@ -17,7 +21,10 @@ class ShowMap:
         maxY=max(tmpY)
         self.frame =  [[self.emptyChr for x in range(maxX-minX+2)] for y in range(maxY-minY+2)]
         for pos,val in data.items():
-            self.frame[pos[1]-minY][pos[0]-minX]=self.legend[val]
+            if self.useAscii:
+                self.frame[pos[1]-minY][pos[0]-minX]=chr(val)
+            else:
+                self.frame[pos[1]-minY][pos[0]-minX]=self.legend[val]
     
     def draw(self):
         for data in self.frame:
